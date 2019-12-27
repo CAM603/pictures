f5ba3e5156545aaa7574ac2499caab2ff26fd3f2
CAM603 <cameronhawley@comcast.net>
2019-12-27 17:47:06 -0500

    added api folder

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import axios from 'axios';
+import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
+
     state = { images: [] };
+
     onSearchSubmit = (term) => {
-        axios
-        .get('https://api.unsplash.com/search/photos', {
+        
+        unsplash
+        .get('/search/photos', {
             params: { query: term },
-            headers: {
-            Authorization: 'Client-ID 727e6fd0ce8c2d1d2c7a9df3379e19b96a6d3e2e5f15207a5587f040b470d8bb'
-            }
         })
         .then(res => this.setState({images: res.data.results }))
         .catch(err => console.log(err))
